7e36ccf0fb8fc8a21e21d6ba44190c9a2602c14f
imran-basha <imranbasha@10decoders.in>
2021-05-18 22:12:27 +0530

    delete, getById, getBtProperty api calls

--- src/Route/Routers.ts
+++ src/Route/Routers.ts
@@ -12,7 +12,7 @@ interface TicketObject {
     status: string
 }
 
-router.post('/createTicket',(req: Request, res: Response)=>{
+router.post('/createTicket', async(req: Request, res: Response)=>{
     const createTicket = new ticketTemp({
         movieName: req.body.movieName,
         movieStartTime: req.body.movieStartTime,
@@ -20,52 +20,70 @@ router.post('/createTicket',(req: Request, res: Response)=>{
         status: req.body.status
     });
 
-    createTicket.save()
+    await createTicket.save()
     .then((data: TicketObject)=>{
-        console.log('SUCCESS 1');
         res.json(data);
     })
     .catch((error: any)=>{
-        console.log('FAILURE ', error);
-        res.json(error);
+        res.status(500).send(error);
     })
 })
-router.delete('/ticket/:id',(req: Request, res: Response)=>{
-    const createTicket = new ticketTemp({
-        movieName: req.body.movieName,
-        movieStartTime: req.body.movieStartTime,
-        movieEndTime: req.body.movieEndTime,
-        status: req.body.status
-    });
-    
-    createTicket.save()
-    .then((data: TicketObject)=>{
-        console.log('SUCCESS 1');
-        res.json(data);
+router.delete('/ticket/:id',async (req: Request, res: Response)=>{
+    const id = await req.params.id;
+    await ticketTemp.findByIdAndDelete(id)
+    .then((ticket)=>{
+        if(! ticket){
+            return res.status(404).send();
+        }
+        res.send(ticket);
     })
-    .catch((error: any)=>{
-        console.log('FAILURE ', error);
-        res.json(error);
+    .catch((error)=>{
+        res.status(500).send(error);
     })
 })
-// router.post('/createTicket',(req: Request, res: Response)=>{
-//     const createTicket = new ticketTemp({
-//         movieName: req.body.movieName,
-//         movieStartTime: req.body.movieStartTime,
-//         movieEndTime: req.body.movieEndTime,
-//         status: req.body.status
-//     });
+router.get('/ticket/:id',async (req: Request, res: Response)=>{
+    const id = await req.params.id;
+    await ticketTemp.findById(id)
+    .then((ticket)=>{
+        if(! ticket){
+            return res.status(404).send();
+        }
+        res.send(ticket);
+    })
+    .catch((error)=>{
+        res.status(500).send(error);
+    })
+})
+router.get('/tickets',async (req: Request, res: Response)=>{
+    const status = await req.query.status;
+    console.log(status);
+    await ticketTemp.find({ "status": status })
+    .then((tickets)=>{
+        if(! tickets){
+            return res.status(404).send();
+        }
+        res.send(tickets);
+    })
+    .catch((error)=>{
+        res.status(500).send(error);
+    })
+})
+router.put('/ticket/:id',async (req: Request, res: Response)=>{
+    const id = await req.params.id;
+    const status = await req.body.status;
 
-//     createTicket.save()
-//     .then((data: TicketObject)=>{
-//         console.log('SUCCESS 1');
-//         res.json(data);
-//     })
-//     .catch((error: any)=>{
-//         console.log('FAILURE ', error);
-//         res.json(error);
-//     })
-// })
-
+    await ticketTemp.findOneAndUpdate(id, { "status": status })
+    .then((ticket)=>{
+        console.log('succ', ticket)
+        if(! ticket){
+            return res.status(404).send();
+        }
+        res.send(ticket);
+    })
+    .catch((error)=>{
+        console.log('fail')
+        res.status(500).send(error);
+    })
+})
 
 module.exports = router;
--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,8 @@ var routeURLs = require('./Route/Routers');
 
 dotenv.config();
 
-// const DATABASE_URL = "mongodb+srv://imran:imran@cluster0.fttr1.mongodb.net/ticketTable?retryWrites=true&w=majority";
 const DATABASE_URL = "mongodb://imran:imran@cluster0-shard-00-00.fttr1.mongodb.net:27017,cluster0-shard-00-01.fttr1.mongodb.net:27017,cluster0-shard-00-02.fttr1.mongodb.net:27017/ticketTable?ssl=true&replicaSet=atlas-ncixya-shard-0&authSource=admin&retryWrites=true&w=majority"
-console.log('url', DATABASE_URL);
-mongoose.connect(DATABASE_URL,{ useUnifiedTopology: true, useNewUrlParser: true }, ()=>console.log('Databse Connected'));
+mongoose.connect(DATABASE_URL,{ useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false }, ()=>console.log('Databse Connected'));
 
 const app = express();
 app.use(express.json());

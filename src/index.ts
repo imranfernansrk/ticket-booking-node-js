4abf9aa456636a3becdc550f3975f09b760cb9d2
imran-basha <imranbasha@10decoders.in>
2021-05-20 21:37:19 +0530

    lastest code

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import * as mongoose from 'mongoose';
 import { Log } from "./Logger/Logger";
 require("dotenv").config();
 
+const PORT = process.env.PORT || 4000;
 const Logs = new Log();
 class App{
     expressApp: Application;
@@ -26,7 +27,6 @@ class App{
         this.expressApp.use(cors({ credentials: true, origin: true , }));
     }
     portSetup() {
-        const PORT = process.env.PORT || 4000;
         http.createServer(this.expressApp).listen(PORT);
     }
     mongoSetup() {
